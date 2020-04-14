4bc0386dd899d51f517c4b031c33c8eb3e300324
jacob kleiman <jacobakleiman@gmail.com>
2020-04-14 16:34:09 -0700

    removes hooks from routes component

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Link, Switch, Redirect} from 'react-router-dom';
 import VideoChat from '.././VideoChat';
 // import About from '.././About';
